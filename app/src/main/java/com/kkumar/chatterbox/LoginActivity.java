008d74952c0c56be810c27d20bea1878a1a0b7b6
kshitiz-kumar <kkrajput09@gmail.com>
2019-02-18 13:28:49 +0530

    UPDATED LOGIN METHOD

--- app/src/main/java/com/kkumar/chatterbox/LoginActivity.java
+++ app/src/main/java/com/kkumar/chatterbox/LoginActivity.java
@@ -3,6 +3,7 @@ package com.kkumar.chatterbox;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.provider.ContactsContract;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
@@ -21,92 +22,39 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
 
-    private EditText inputEmail, inputPassword,takenumber;
+    private EditText takenumber;
     private FirebaseAuth auth;
     private ProgressBar progressBar;
-    private Button btnSignup, btnLogin, btnReset;
+    private Button btnLogin;
 
 
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        //shared preferences to keep user logged in
-        SharedPreferences pref = getApplicationContext().getSharedPreferences("MyPref", MODE_PRIVATE);
-        if(pref.getBoolean("isLoggedin",false))
-        {
-            Intent intent=new Intent(LoginActivity.this,MainInterfaceActivity.class);
-            startActivity(intent);
-            finish();
-        }
+
         //set view now
         setContentView(R.layout.activity_login);
-        inputEmail = (EditText) findViewById(R.id.email);
-        inputPassword = (EditText) findViewById(R.id.password);
-        progressBar = (ProgressBar) findViewById(R.id.progressBar);
-        btnSignup = (Button) findViewById(R.id.btn_signup);
         btnLogin = (Button) findViewById(R.id.btn_login);
-        btnReset = (Button) findViewById(R.id.btn_reset_password);
-
         takenumber=(EditText)findViewById(R.id.takenumber);
 
-        //fireBase Auth Instance
-        auth=FirebaseAuth.getInstance();
+    }
 
-        btnReset.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(LoginActivity.this,ResetPasswordActivity.class));
-                }});
+    public void verify(View view)
+    {
 
-        btnLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String email= inputEmail.getText().toString().trim();
-                final String password= inputPassword.getText().toString().trim();
-                final String phone=takenumber.getText().toString().trim();
-
-                if(TextUtils.isEmpty(email)){
-                    Toast.makeText(getApplicationContext(),"Enter email Address!",Toast.LENGTH_SHORT).show();
-                    return;
-                }
-                if(TextUtils.isEmpty(password)){
-                    Toast.makeText(getApplicationContext(),"Enter password!",Toast.LENGTH_SHORT).show();
-                    return;
-                }
-
-                auth.signInWithEmailAndPassword(email,password).addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                      progressBar.setVisibility(View.GONE);
-                      if(!task.isSuccessful()){
-                          if(password.length()<6){
-                              inputPassword.setError(getString(R.string.minimum_password));
-                          }else{
-                              Toast.makeText(LoginActivity.this,getString(R.string.auth_failed),Toast.LENGTH_SHORT).show();
-                          }
-                      }
-                      else{
-
-                          SharedPreferences pref = getApplicationContext().getSharedPreferences("MyPref", MODE_PRIVATE);
-                          SharedPreferences.Editor editor = pref.edit();
-                          editor.putBoolean("isLoggedin", true);
-                          editor.putString("Phone", phone);
-                          editor.apply();
-
-                          Intent intent=new Intent(LoginActivity.this,MainInterfaceActivity.class);
-                          startActivity(intent);
-                          finish();
-                      }
-                    }
-                });
-            }
-        });
-
-        btnSignup.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent=new Intent(LoginActivity.this,SignupActivity.class);
-                startActivity(intent);
-            }
-        });
+        if(takenumber.getText().toString().length()!=10)
+        {
+            Toast.makeText(LoginActivity.this, "Invalid Phone number", Toast.LENGTH_SHORT).show();
+            takenumber.setError("Invalid Number");
+            return;
+        }
+        else
+        {
+            SharedPreferences preference = PreferenceManager.getDefaultSharedPreferences(LoginActivity.this);
+            SharedPreferences.Editor editor = preference.edit();
+            editor.putString("UserPhone",takenumber.getText().toString());
+            editor.apply();
+            Intent intent=new Intent(LoginActivity.this,OtpVerification.class);
+            startActivity(intent);
+        }
     }
 }
---
+++ app/src/main/java/com/kkumar/chatterbox/OtpVerification.java
@@ -0,0 +1,227 @@
+package com.kkumar.chatterbox;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.TypedValue;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.concurrent.TimeUnit;
+
+public class OtpVerification extends AppCompatActivity {
+
+    EditText editTextCode;
+    FirebaseAuth mAuth;
+    String codeSent;
+    PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks;
+    String code,phone;
+    ProgressDialog pd;
+    Button resend;
+    TextView otpnumber;
+    Button confirm;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_otp_verification);
+
+        mAuth = FirebaseAuth.getInstance();
+        editTextCode = findViewById(R.id.code);
+        resend =(Button)findViewById(R.id.resendcode);
+        otpnumber=(TextView)findViewById(R.id.otpnumber);
+        confirm=(Button)findViewById(R.id.confirm);
+
+
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        phone = preferences.getString("UserPhone", "");
+        otpnumber.setText("+91 "+phone);
+
+
+
+        pd = new ProgressDialog(this);
+        pd.setCancelable(false);
+        pd.setMessage("Verifying....");
+
+        resend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
+
+        mCallbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+            @Override
+            public void onCodeSent(String s, PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+                super.onCodeSent(s, forceResendingToken);
+
+                confirm.setEnabled(true);
+                editTextCode.setEnabled(true);
+                confirm.setBackgroundResource(R.drawable.rounded_button);
+                codeSent = s;
+
+            }
+
+            @Override
+            public void onVerificationCompleted(PhoneAuthCredential phoneAuthCredential) {
+
+                code = phoneAuthCredential.getSmsCode();
+                if (code != null) {
+
+                    editTextCode.setTextSize(TypedValue.COMPLEX_UNIT_SP, 20f);
+                    editTextCode.setText(code);
+                    verifySignInCode(code);
+                    pd.show();
+                }
+
+            }
+
+            @Override
+            public void onVerificationFailed(FirebaseException e) {
+                Toast.makeText(getApplicationContext(),
+                        e.getMessage(), Toast.LENGTH_LONG).show();
+                onBackPressed();
+                finish();
+
+            }
+
+
+        };
+
+        // confirm
+
+        findViewById(R.id.confirm).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                code = editTextCode.getText().toString();
+                if(code.equals(""))
+                    Toast.makeText(OtpVerification.this, "Field can't be empty", Toast.LENGTH_SHORT).show();
+                else {
+                    verifySignInCode(code);
+                    pd.show();
+                }
+
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        sendVerificationCode();
+    }
+
+    private void verifySignInCode(String code) {
+        // String code = editTextCode.getText().toString();
+        PhoneAuthCredential credential = PhoneAuthProvider.getCredential(codeSent, code);
+        signInWithPhoneAuthCredential(credential);
+    }
+
+    private void signInWithPhoneAuthCredential(PhoneAuthCredential credential) {
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            //here you can open new activity
+
+                            FirebaseDatabase database = FirebaseDatabase.getInstance();
+                            DatabaseReference myRef = database.getReference("Users").child(phone);
+
+                            myRef.addValueEventListener(new ValueEventListener() {
+                                @Override
+                                public void onDataChange(DataSnapshot dataSnapshot) {
+                                    if(dataSnapshot.exists())
+                                    {
+                                        pd.dismiss();
+
+                                        SharedPreferences preference = PreferenceManager.getDefaultSharedPreferences(OtpVerification.this);
+                                        SharedPreferences.Editor editor = preference.edit();
+                                        editor.putBoolean("isloggedin", true);
+                                        editor.apply();
+
+                                        Intent intent=new Intent(OtpVerification.this,MainInterfaceActivity.class);
+                                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                                        startActivity(intent);
+                                    }
+                                    else
+                                    {
+                                        pd.dismiss();
+
+                                        Intent intent=new Intent(OtpVerification.this,MainInterfaceActivity.class);
+                                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                                        startActivity(intent);
+                                    }
+                                }
+
+                                @Override
+                                public void onCancelled(DatabaseError error) {
+                                    // Failed to read value
+                                }
+                            });
+
+
+
+                        } else {
+                            if (task.getException() instanceof FirebaseAuthInvalidCredentialsException) {
+                                pd.dismiss();
+                                Toast.makeText(getApplicationContext(),
+                                        "Incorrect Verification Code ", Toast.LENGTH_LONG).show();
+                            }
+                        }
+                    }
+                });
+    }
+
+    private void sendVerificationCode() {
+
+        PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                "+91" + phone,        // Phone number to verify
+                60,                 // Timeout duration
+                TimeUnit.SECONDS,   // Unit of timeout
+                OtpVerification.this,              // Activity (for callback binding)
+                mCallbacks);        // OnVerificationStateChangedCallbacks
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+    }
+
+}
